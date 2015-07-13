1c9043878134722cf6a9b50ca543260ade416b80
Santiago Salas <santiago.salas.v@hotmail.de>
2015-07-12 21:04:55 -0500

    added requirements of input csv

--- fit_parameters_to_data.py
+++ fit_parameters_to_data.py
@@ -19,32 +19,36 @@ def fit_from_csv(file_name):
     ext = os.path.splitext(os.path.split(file_name)[-1])[-1]
     single_name = os.path.splitext(os.path.split(file_name)[-1])[0]
     data_table = np.genfromtxt(file_name, dtype=['S6', 'float', 'float'],
-                               delimiter=',', skip_header=1, usecols=range(3),
-                               names=['sample', 'days', 'penetrability_mm'])
-    categories = np.unique(data_table['sample'])
-    params_list = dict()
-    with open(single_name + '_with_fits' + ext, 'w') as outfile:
-        writer = csv.writer(outfile, lineterminator='\n')
-        writer.writerow(['sample', 'days', 'penetrability_mm']
-                        + [param_letters[y] for y in range(4)]
-                        + ['R^2', 'Calc(penetrability_mm)'])
-        for k in categories:
-            popt1, pcov1 = optimize.curve_fit(f,
-                                              data_table[data_table['sample'] == k]['days'],
-                                              data_table[data_table['sample'] == k]['penetrability_mm'])
-            params_list[k] = popt1
-            for j in data_table[data_table['sample'] == k]:
-                j_list = list(j)
-                if ~ np.isinf(np.inf):
-                    writer.writerow(j_list
-                                    + popt1.tolist()
-                                    + np.sqrt(np.diag(pcov1)).tolist())
-                else:
-                    writer.writerow(j_list
-                                    + popt1.tolist()
-                                    + [np.inf])
-        del y, k, j
-    return params_list
+                               delimiter=',', skip_header=0, usecols=range(3),
+                               names=True)
+    if data_table.dtype.names != ('sample', 'days', 'penetrability_mm'):
+        return "Invalid file. File must have columns: " \
+               "('sample', 'days', 'penetrability_mm')"
+    else:
+        categories = np.unique(data_table['sample'])
+        params_list = dict()
+        with open(single_name + '_with_fits' + ext, 'w') as outfile:
+            writer = csv.writer(outfile, lineterminator='\n')
+            writer.writerow(['sample', 'days', 'penetrability_mm']
+                            + [param_letters[y] for y in range(4)]
+                            + ['R^2', 'Calc(penetrability_mm)'])
+            for k in categories:
+                popt1, pcov1 = optimize.curve_fit(f,
+                                                  data_table[data_table['sample'] == k]['days'],
+                                                  data_table[data_table['sample'] == k]['penetrability_mm'])
+                params_list[k] = popt1
+                for j in data_table[data_table['sample'] == k]:
+                    j_list = list(j)
+                    if ~ np.isinf(np.inf):
+                        writer.writerow(j_list
+                                        + popt1.tolist()
+                                        + np.sqrt(np.diag(pcov1)).tolist())
+                    else:
+                        writer.writerow(j_list
+                                        + popt1.tolist()
+                                        + [np.inf])
+            del y, k, j
+        return params_list
 
 
 (filename, _) = \
@@ -53,4 +57,7 @@ def fit_from_csv(file_name):
                                       dir=os.path.splitdrive(sys.path[0])[0],
                                       filter='*.csv')
 
-fit_from_csv(filename)
+return_value = fit_from_csv(filename)
+
+# exit 'main loop'
+sys.exit(app.exec_())
