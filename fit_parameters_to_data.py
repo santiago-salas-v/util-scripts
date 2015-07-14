b35c2c3c20eeb1cb66ef754058f5c1b9ef24defa
Santiago Salas <santiago.salas.v@hotmail.de>
2015-07-14 00:44:40 -0500

    add msgBox

--- fit_parameters_to_data.py
+++ fit_parameters_to_data.py
@@ -23,16 +23,17 @@ def fit_from_csv(file_name):
     data_table = np.genfromtxt(file_name, dtype=['S6', 'float', 'float'],
                                delimiter=',', skip_header=0, usecols=range(3),
                                names=True)
+    out_file_name = single_name + '_with_fits' + ext
     if data_table.dtype.names != ('sample', 'days', 'penetrability_mm'):
         return "Invalid file. File must have columns: " \
                "('sample', 'days', 'penetrability_mm')"
     else:
         categories = np.unique(data_table['sample'])
         params_list = dict()
-        with open(single_name + '_with_fits' + ext, 'w') as outfile:
+        with open(out_file_name, 'w') as outfile:
             writer = csv.writer(outfile, lineterminator='\n')
             writer.writerow(['sample', 'days', 'penetrability_mm']
-                            + [param_letters[y] for y in range(len(inspect.getargspec(f))-1)]
+                            + [param_letters[y] for y in range(len(inspect.getargspec(f)))]
                             + ['R^2', 'Calc(penetrability_mm)'])
             for k in categories:
                 popt1, pcov1 = optimize.curve_fit(f,
@@ -41,16 +42,16 @@ def fit_from_csv(file_name):
                 params_list[k] = popt1
                 for j in data_table[data_table['sample'] == k]:
                     j_list = list(j)
-                    if ~ np.isinf(pcov1):
-                        writer.writerow(j_list
-                                        + popt1.tolist()
-                                        + np.sqrt(np.diag(pcov1)).tolist())
-                    else:
+                    if np.isinf(pcov1).any():
                         writer.writerow(j_list
                                         + popt1.tolist()
                                         + [np.inf])
+                    else:
+                        writer.writerow(j_list
+                                        + popt1.tolist()
+                                        + np.sqrt(np.diag(pcov1)).tolist())
             del y, k, j
-        return params_list
+        return params_list, out_file_name
 
 
 (filename, _) = \
@@ -58,8 +59,17 @@ def fit_from_csv(file_name):
                                       caption='Open file',
                                       dir=os.path.splitdrive(sys.path[0])[0],
                                       filter='*.csv')
+
 if filename != '':
-    return_value = fit_from_csv(filename)
+    return_value, out_file_name = fit_from_csv(filename)
+
+msgBox = QtGui.QMessageBox()
+msgBox.setText('Saved: ' + os.path.dirname(filename) + out_file_name)
+msgBox.setWindowTitle('Saved file.')
+msgBox.setStandardButtons(QtGui.QMessageBox.Close)
+QtCore.QObject.connect(msgBox.button(QtGui.QMessageBox.Close),
+                       QtCore.SIGNAL('clicked()'), msgBox.close)
 
 # exit 'main loop'
-sys.exit(app.exec_())
+sys.exit(msgBox.exec_())
+#sys.exit(app.exec_())
